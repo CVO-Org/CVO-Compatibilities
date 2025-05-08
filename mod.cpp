e4b22d6e62a30c65d2c925fece6e523515708a24
SkippieDippie <skippiedipie@gmail.com>
2025-05-08 22:23:52 +0200

    Getting ready for release

---
+++ mod.cpp
@@ -0,0 +1,9 @@
+name            = "24th CVO Compatibilities";
+author          = "24th Chorni Voron";
+logo            = "logo_cvo_ca.paa";
+logoOver        = "logo_cvo_ca.paa";
+tooltip         = "24th CVO Compatibilities";
+picture         = "logo_cvo_compats_ca.paa";
+actionName      = "GitHub";
+action          = "https://github.com/SkippieDippie/CVO-Everything-Compats";
+overview        = "<t size='2.5'>24th CVO Compatibilities</t>"This mod aims to make certain mods compatible with mods like ACRE, GreenMag and possibly other.<br/><br/>Be aware that this mod was primarily created for the 24th Chorni Voron community so the mod will be balanced around this.<br/><br/><t size='2'>Content of the Mod</t><br/><br/><t size='1.5'>GreenMag Compatibilities:</t><br/>  - RHS AFRF <br/>  - RHS USAF<br/>  - RHS GREF<br/>  - RHS SAF<br/>  - 3CB Factions<br/>  - SOG Praire Fire<br/><br/><t size='1.5'>ACRE Compatibilities:</t><br/>  - 3CB Factions (Partial)<br/>  - SOG Praire Fire (Partial)";
