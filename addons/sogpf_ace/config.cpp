d24933f05575e49d7d55692eaecdae960f689374
SkippieDippie <skippiedipie@gmail.com>
2025-05-10 11:52:35 +0200

    General addon structure

---
+++ addons/sogpf_ace/config.cpp
@@ -0,0 +1,22 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = "24th CVO Compatibilities - SOG Praire Fire ACE3";
+        author = ELSTRING(main,author);
+        url = "https://github.com/SkippieDippie/CVO-Everything-Compats";
+
+        requiredAddons[] = {
+            "air_f_vietnam_c",
+            "air_f_vietnam_02_c",
+            "ace_main"
+        };
+        requiredVersion = 1.00;
+        skipWhenMissingDependencies = 1;
+
+        units[] = {};
+        weapons[] = {};
+    };
+};
+
+#include "CfgVehicles.hpp"
